763ad8dce565f53122033d42a91cf0f29243da1a
Fabio Franklin <ffranklinbarbosa@apple.com>
2022-01-28 15:29:13 -0300

    Atividade aulas 3 e 4: Page Object + Cypress test Parte 1: Crie um formulário para adicionar uma nova skill Parte 2: Crie um page object para esse formulário no arquivo de testes dentro do app react. Escreva um arquivo teste para esse componente usando o Cypress - Resta ajustar classe app.test

--- Aula 4/ui-testing/cypress/integration/App.spec.js
+++ Aula 4/ui-testing/cypress/integration/App.spec.js
@@ -1,3 +1,4 @@
+
 describe('App Developers Skills', () => {
 
     beforeEach(() => {
@@ -16,4 +17,56 @@ describe('App Developers Skills', () => {
         cy.contains('Skill Name: front-end');
     });
 
-});
+    it('Should acess the Add Skills form and assert all components',() => {
+        cy.get('#add-skill-button').click();
+        cy.get('h5').then(($option) => {
+            expect($option.eq(0)).to.have.text(' Skill Name ')
+            $option.eq(0)
+            expect($option.eq(1)).to.have.text(' Developers ')
+            $option.eq(1)
+            expect($option.eq(2)).to.have.text(' Thecnologies ')
+            $option.eq(2)
+            expect($option.eq(3)).to.have.text(' Roles ')
+            $option.eq(3)
+        cy.get('#skillName').should('be.visible');
+        cy.get('#developers').should('be.visible');
+        cy.get('select#developers')
+          .should('contain','Dev_1')
+          .should('contain','Dev_2')
+          .should('contain','Dev_3')
+        cy.get('#technologies').should('be.visible');
+        cy.get('#technologies')
+          .should('contain','Java')
+          .should('contain','React')
+          .should('contain','Phyton')
+        cy.get('#roles').should('be.visible');
+        cy.get('#roles')
+          .should('contain','Developer')
+          .should('contain','Quality Assurance')
+          .should('contain','UI/UX Designer')
+          cy.get('button[type="submit"]').should('be.visible')
+    });
+  });
+
+    it('Should add a new skill',() => {
+            cy.get('#add-skill-button').click();
+            cy.get('#skillName').type('Full Stack');
+            cy.get('select[name="developers"]').select('Dev_2')
+            cy.get('select#technologies').select('React')
+            cy.get('#roles').select('Quality Assurance')
+             cy.get('button[type="submit"').click();
+    })
+    it('Should Load a new Added Skill',() => {
+        cy.get('#add-skill-button').click();
+        cy.get('#skillName').type('Full Stack');
+        cy.get('select[name="developers"]').select('Dev_2')
+        cy.get('select#technologies').select('React')
+        cy.get('#roles').select('Quality Assurance')
+        cy.get('button[type="submit"').click();
+        cy.contains('Skill Name: Full Stack')
+        cy.contains('Developers:Dev_2')
+        cy.contains('Technologies:React')
+        cy.contains('Roles:Quality Assurance')
+       
+    });
+})
--- Aula 4/ui-testing/src/App.tsx
+++ Aula 4/ui-testing/src/App.tsx
@@ -1,10 +1,12 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import './App.css';
 import { useState } from 'react';
-
+interface skillsDataTypes{skillName:string,developers:string[],profile:{technologies:string[],roles:string[]}}
 const App = () => {
+const [typedData,setTypedData] = useState<skillsDataTypes>()
+const [ShowAddForm,setShowAddForm]=useState(false)
 
-  const [skillData, setSkillData] = useState({
+  const [skillData, setSkillData] = useState<skillsDataTypes>({
     "skillName": "",
     "developers": [],
     "profile": {
@@ -12,7 +14,7 @@ const App = () => {
       "roles": []
     }
   });
-  const [skillsData, setSkillsData] = useState([{
+  const [skillsData, setSkillsData] = useState<skillsDataTypes[]>([{
     "skillName": "",
     "developers": [],
     "profile": {
@@ -39,6 +41,12 @@ const App = () => {
           <li>{skill.profile.technologies[1]}</li>
           </ul>
         </li>
+        <li>Roles:
+      <ul>
+        <li>{skill.profile.roles[0]}</li>
+        <li>{skill.profile.roles[1]}</li>
+      </ul>
+    </li>
       </ul>);
 
   let name = <><h3>{skillData.skillName}</h3>
@@ -54,6 +62,12 @@ const App = () => {
       <ul>
         <li>{skillData.profile.technologies[0]}</li>
         <li>{skillData.profile.technologies[1]}</li>
+      </ul>
+    </li>
+    <li>Roles:
+      <ul>
+        <li>{skillData.profile.roles[0]}</li>
+        <li>{skillData.profile.roles[1]}</li>
       </ul>
     </li>
   </ul></>;
@@ -76,7 +90,25 @@ const App = () => {
         setSkillsData(data);
         });
   }
+ function OnSubmit(e:any) {
+ const currentSkillsData = skillsData
+ currentSkillsData.push(skillData)
+ setSkillsData(currentSkillsData)
+ setShowSkills(true)
+ setShowAddForm(false)
+ }
+ function OnChange(e:any,fieldName:string) {
+   console.log(e.target.value)
+   if (fieldName ==="technologies"|| fieldName ==="roles"){
+     setSkillData({...skillData,profile:{...skillData.profile,[fieldName]:[e.target.value]}})
+   } else if (fieldName ==="developers"){setSkillData({...skillData,developers:[e.target.value]})}
+     else {{setSkillData({...skillData,[fieldName]:e.target.value})}
+ }
+}
+function HandleClickAddBtn(){
+setShowAddForm(true)
 
+}
   return (
       <div className="App">
         <header className="App-header">
@@ -84,14 +116,62 @@ const App = () => {
             Developers App
           </h1>
       </header>
-      <label htmlFor="input1">Type the skill you would like to search</label>
-        <input id="input-skill" className='input1'></input>
-      <a id="search-button" className="button1" onClick={() => loadSkill()}>Search</a>
-        <a id="load-skills-button" className="button1" onClick={() => loadSkills()}>All Skills</a>
-          {showSkill? name : ''}
-          {showSkills? listSkills: ''}
-    </div>
+      {!ShowAddForm?
+
+       (<><label htmlFor="input1">Type the skill you would like to search</label>
+       <input id="input-skill" className='input1'></input>
+     <a id="search-button" className="button1" onClick={() => loadSkill()}>Search</a>
+       <a id="load-skills-button" className="button1" onClick={() => loadSkills()}>All Skills</a>
+       <a id="add-skill-button" className="button1" onClick={HandleClickAddBtn}>Add Skills</a>
+         {showSkill? name : ''}
+         {showSkills? listSkills: ''}</>):(<form onSubmit= {OnSubmit}> 
+          <body>
+          <h5> Skill Name </h5>
+          <input type="text" id="skillName" name="skillName" onChange= {(e)=>OnChange(e,"skillName")}style={{width:"200px", height:"40px"}}/>
+          <h5> Developers </h5>
+          <select id="developers" name="developers" onChange= {(e)=>OnChange(e,"developers")}style={{width:"200px", height:"40px"}}>
+            <input type ="text" name="Developer"/>
+            <option value="Dev_1">
+            Dev_1
+            </option>
+            <option value="Dev_2">
+            Dev_2
+            </option>
+            <option value="Dev_3">
+            Dev_3
+            </option>
+          </select>
+          <h5> Thecnologies </h5>
+          <select id="technologies" name="technologies" onChange= {(e)=>OnChange(e,"technologies")}style={{width:"200px", height:"40px"}}>
+            <option value="Java">
+            Java
+            </option>
+            <option value="React">
+            React
+            </option>
+            <option value="Phyton">
+            Phyton
+            </option>
+            </select>
+            <h5> Roles </h5>
+            <select id="roles" name="roles" onChange= {(e)=>OnChange(e,"roles")}style={{width:"200px", height:"40px"}}>
+            <option value="Developer">
+            Developer
+            </option>
+            <option value="Quality Assurance">
+            Quality Assurance
+            </option>
+            <option value="UI/UX_Designer">
+            UI/UX Designer
+            </option>
+            </select>
+            </body>
+          <button type="submit">Add Skills </button></form>)
+       }
+       
+           
+      </div>
+      
     );
 }
-
 export default App;
